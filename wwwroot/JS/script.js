f0c7e9d3983f87a0e744139edc55f2504de77bf6
Dai Dong <phungdaidong1114@gmail.com>
2024-01-15 23:53:51 +0700

    dong about

--- wwwroot/JS/script.js
+++ wwwroot/JS/script.js
@@ -14,3 +14,17 @@ $(document).ready(function () {
 
 });
 // ends
+
+let layout_header_icon = $("#layout-header-icon");
+let dropdown_content_element = $(".dropdown-content");
+window.addEventListener("scroll", function(){
+    if(this.window.scrollY > 60){
+        layout_header_icon.css("animation","smaller 0.3s linear");
+        layout_header_icon.css("width","5rem");
+        dropdown_content_element.css("top","50px");
+    }else{
+        layout_header_icon.css("animation","bigger 0.3s linear");
+        layout_header_icon.css("width","8rem");
+        dropdown_content_element.css("top","80px");
+    }
+})
